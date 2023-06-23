92c7bb90a4ce3868b9be82467523d28720ef1b51
Tanni-Dey <tonnidey277615@gmail.com>
2023-06-23 15:08:33 +0600

    model

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
     // await mongoose.connect("mongodb://127.0.0.1:27017/test");
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`university mangment listening on port ${config.port}`);
-    });
+      console.log(`university mangment listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(`Application error ${err}`);
+    console.log(`Application error ${err}`)
   }
 }
-main();
+main()
